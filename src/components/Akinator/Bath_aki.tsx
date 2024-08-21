16ea2ef21e16ffed8ef79094042ef37f4bae1754
Momoka <hirose.momoka.hm4@naist.ac.jp>
2024-08-21 13:01:08 +0900

    [fix]akiのスタイル統一

--- src/components/Akinator/Bath_aki.tsx
+++ src/components/Akinator/Bath_aki.tsx
@@ -1,20 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { collection, doc, setDoc, serverTimestamp } from "firebase/firestore";
-import { auth, db } from "../../firebase/firebase"; // Import the initialized Firestore instance
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
+import { auth, db } from "../../firebase/firebase";
 import { onAuthStateChanged } from "firebase/auth";
-import { useNavigate } from "react-router-dom"; // useNavigateをインポート
-import Akinator from "/image/akinator.png";
+import { useNavigate } from "react-router-dom";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
-import useViewportHeight from "../../hooks/useViewportHeight"; // Import the custom hook
+import useViewportHeight from "../../hooks/useViewportHeight"; 
+import styles from './CommonStyles';
 
 const BathAki: React.FC = () => {
   const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
   const [user, setUser] = useState<any>(null);
   const [showCheckmark, setShowCheckmark] = useState<boolean>(false);
-  const navigate = useNavigate(); // useNavigateフックを使用
-  const viewportHeight = useViewportHeight(); // Use the custom hook
+  const navigate = useNavigate();
+  const viewportHeight = useViewportHeight();
 
-  // Monitor auth state
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -61,7 +60,7 @@ const BathAki: React.FC = () => {
       setTimeout(() => {
         setShowCheckmark(false);
         navigate("/food");
-      }, 500); // 0.5秒後に次のページに遷移
+      }, 500);
     } catch (error) {
       console.error("Error saving selected options: ", error);
     }
@@ -78,11 +77,11 @@ const BathAki: React.FC = () => {
         ) : (
           <>
             <div style={styles.questionContainer}>
-              
-              <img src="/image/akinator.png" alt="Akinator" 
-              style={{ width: "85%", height: "90%" }}
+              <img 
+                src="/image/akinator.png" 
+                alt="Akinator" 
+                style={styles.akinatorImage}
               />
-
               <h2 style={styles.question}>質問1/4:</h2>
               <p style={styles.subQuestion}>
                 お風呂にいつ入りますか？（複数選択可）
@@ -104,7 +103,10 @@ const BathAki: React.FC = () => {
                 </button>
               ))}
             </div>
-            <button style={styles.submitButton} onClick={handleSubmit}>
+            <button 
+              style={styles.submitButton} 
+              onClick={handleSubmit}
+            >
               決定
             </button>
           </>
@@ -112,79 +114,6 @@ const BathAki: React.FC = () => {
       </main>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    display: "flex",
-    flexDirection: "column" as "column",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#1d3557",
-    color: "#f4a261",
-  },
-  header: {
-    width: "100%",
-    textAlign: "center" as "center",
-    backgroundColor: "#1d3557",
-    padding: "10px 0",
-  },
-  title: {
-    fontSize: "2.5em",
-    margin: "0",
-  },
-  main: {
-    display: "flex",
-    flexDirection: "column" as "column",
-    alignItems: "center",
-    backgroundColor: "#f1faee",
-    padding: "20px",
-    borderRadius: "10px",
-  },
-  questionContainer: {
-    textAlign: "center" as "center",
-    marginBottom: "20px",
-  },
-  question: {
-    fontSize: "1.5em",
-    margin: "0",
-    color: "#000",
-  },
-  subQuestion: {
-    fontSize: "1em",
-    margin: "0",
-    color: "#000",
-  },
-  optionsContainer: {
-    display: "flex",
-    flexDirection: "column" as "column",
-    width: "100%",
-    alignItems: "center",
-  },
-  optionButton: {
-    width: "80%",
-    padding: "10px 0",
-    margin: "10px 0",
-    border: "none",
-    borderRadius: "5px",
-    cursor: "pointer",
-    fontSize: "1em",
-    color: "#000",
-  },
-  submitButton: {
-    backgroundColor: "#f4a261",
-    color: "#1d3557",
-    border: "none",
-    padding: "10px 20px",
-    margin: "20px 0 0 0",
-    fontSize: "1em",
-    borderRadius: "5px",
-    cursor: "pointer",
-  },
-  checkmark: {
-    fontSize: "4em",
-    color: "#f4a261",
-  },
 };
 
 export default BathAki;
